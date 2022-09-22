9bbdc405de53c6b8ffad44bf49a2233b0237f3ae
jonas-elias <jonasdasilvaelias@gmail.com>
2022-09-21 22:32:14 -0300

    feat: implement functions to send arrayMessage in whatsapp

---
+++ console-whatsapp.js
@@ -0,0 +1,59 @@
+/**
+ * Method to send message with contain in arrayMessages
+ * 
+ * @param {array} arrayMessages 
+ * @param {integer} interval
+ * @return {void} 
+ */
+async function sendMessage(arrayMessages, interval) {
+    const selectorInputText = 'P.copyable-text.selectable-text';
+    const button = 'button.tvf2evcx.oq44ahr5.lb5m6g5c.svlsagor.p2rjqpw5.epia9gcq'
+    for (let i = 0; i < arrayMessages.length; i++) {
+        const InputText = document.querySelector(selectorInputText);
+        await readMessageInput(InputText, arrayMessages[i], interval);
+        await clickSend(button);
+    }
+}
+
+/**
+ * Method to insert message in input element text of whatsapp
+ * 
+ * @param {HTMLElement} element 
+ * @param {string} message 
+ * @param {integer} interval 
+ * @returns {Promise}
+ */
+function readMessageInput(element, message, interval) {
+    return new Promise((resolved, rejected) => {
+        try {
+            setTimeout(() => {
+                element.focus();
+                if (!document.execCommand('insertText', false, message)) {
+                    element.value = message;
+                }
+                element.dispatchEvent(new Event('change', { bubbles: true }));
+                resolved('resolved');
+            }, interval * 1000);
+        } catch (error) {
+            rejected(error)
+        }
+    })
+}
+
+/**
+ * Method to click in button send (sent message...)
+ * 
+ * @param {string} selectorButton 
+ * @returns {Promise}
+ */
+function clickSend(selectorButton) {
+    return new Promise((resolved, rejected) => {
+        try {
+            document.querySelector(selectorButton).click();
+            console.log('Message sent...');
+            resolved('resolved');
+        } catch (error) {
+            rejected(error)
+        }
+    })
+}
